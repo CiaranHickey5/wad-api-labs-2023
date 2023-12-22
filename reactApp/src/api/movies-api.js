9ac7fbfc0658d12b8cfc09ddaa931951594bb3cc
CiaranHickey5 <ciaran.hickey99@gmail.com>
2023-12-22 05:10:05 +0000

    Topic 12 - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,7 +1,9 @@
 export const getMovies = async () => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=c7e99547767afd89b423d34ba170cbb9&language=en-US&include_adult=false&page=1`
-  );
+  const response = await fetch("http://localhost:8080/api/movies", {
+    headers: {
+      Authorization: window.localStorage.getItem("token"),
+    },
+  });
   return response.json();
 };
 
